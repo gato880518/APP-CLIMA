c97d1978e2a9cecf884bc3c03b8d5e78b0f6b335
Ricardo R <mecanico5749@gmail.com>
2022-11-27 16:51:47 -0500

    texto-final-Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -9,7 +9,7 @@ const Loading = () => {
   return (
     <div>
  <h1>Loading...</h1>
- <h3>Para inicar activa tu Geolocalizacion</h3>
+ <h3>Para inicar activa tu Geolocalizacion y recarga la Pagina</h3>
  
 
     </div>
